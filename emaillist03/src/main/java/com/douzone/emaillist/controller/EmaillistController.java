e4a3cf44159ac9320e461005b8b6ea1857ab201d
R35 <R35@DESKTOP-SL3CU78>
2020-02-21 12:16:07 +0900

    emaillist03 maven Spring적용 완성

--- emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
+++ emaillist03/src/main/java/com/douzone/emaillist/controller/EmaillistController.java
@@ -1,15 +1,37 @@
 package com.douzone.emaillist.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.douzone.emaillist.repository.EmaillistRepository;
+import com.douzone.emaillist.vo.EmaillistVo;
 
 @Controller
 public class EmaillistController {
 	
-	@ResponseBody
-	@RequestMapping
-	public String index() {
-		return "hello";
+	@Autowired
+	private EmaillistRepository emaillistRespository;
+
+	@RequestMapping("")
+	public String index(Model model) {
+		List<EmaillistVo> list = emaillistRespository.findAll();
+		model.addAttribute("list", list);
+		return "index";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.GET)
+	public String add() {
+		return "add";
+	}
+	
+	@RequestMapping(value="/add", method=RequestMethod.POST)
+	public String add(EmaillistVo vo) {
+		emaillistRespository.insert(vo);
+		return "redirect:/";
 	}
 }
