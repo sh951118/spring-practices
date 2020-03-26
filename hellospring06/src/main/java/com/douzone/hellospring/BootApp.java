ba51a0be1a4c7915367d924949ea56a49097978c
R35 <R35@DESKTOP-SL3CU78>
2020-03-26 17:32:34 +0900

    hellospring06 pom수정

--- hellospring06/src/main/java/com/douzone/hellospring/BootApp.java
+++ hellospring06/src/main/java/com/douzone/hellospring/BootApp.java
@@ -3,6 +3,9 @@ package com.douzone.hellospring;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// 밑의 어노테이션이 붙어있는 경우 하위와 package를 맞춰준다.
+// ex) com.douzone.hellospring에 @SpringBootApplication이 있을 경우,
+// 	   com.douzone.hellospring.controller식으로 이름 맞추기!
 @SpringBootApplication
 public class BootApp {
 
